60090d72fc80176de66a0a408246d08e6abc5ee8
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-18 20:57:42 +0100

    dark mode toggle

---
+++ js/darkmode.js
@@ -0,0 +1,16 @@
+/* Dark Mode */
+
+const darkmode = document.querySelector('[data-js="darkmode"]');
+const body = document.querySelector('[data-js="body"]');
+
+darkmode.addEventListener("change", (event) => {
+  document.body.classList.toggle("dark-mode", darkmode.checked);
+});
+
+// const darkmode = document.querySelector('[data-js="darkmode"]');
+// const body = document.querySelector('[data-js="body"]');
+
+// darkmode.addEventListener("click", (event) => {
+//   document.body.classList.add("dark-mode");
+//   console.log("works");
+// });
